b0588e607ad6419b1615e696b405ceff043199f2
agus2409 <agustin.arevalo5@gmail.com>
2014-08-15 14:39:38 -0300

    Segundo Commit

--- code-katas-1/src/code_katas_1/core.clj
+++ code-katas-1/src/code_katas_1/core.clj
@@ -4,12 +4,15 @@
   "Escribir una funcion que retorne solamente los numeros impares de
    una secuencia"
   [s]
-  )
+  (filter odd? s)
+)
 
 (defn nil-key
   "Escribir una funcion que dada una clave y un mapa, devuelva true, solamente si el mapa
    contiene una entrada con esa clave, y su valor es nil"
-  [k m])
+  [k m]
+  (if (contains? m k) (= (get m k) nil) false)
+  )
 
 (defn range
   "Escribir una funcion que cree una lista de enteros en un rango dado.
@@ -61,3 +64,5 @@
    Restricciones: zipmap"
   [k v]
   )
+
+(nil-key :b {:a nil :b 2})
